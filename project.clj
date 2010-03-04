9fd41bde6684a1ae5a4235c8cdc2e319ce4dfafb
David Santiago <david.santiago@gmail.com>
2010-03-04 02:28:28 -0600

    Replaced build.xml with project.clj.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject decorate "0.1.0-SNAPSHOT"
+  :description "A small library for Compojure that allows defined functions
+    to be enhanced by decorator functions."
+  :dev-dependencies [[swank-clojure "1.1.0"]])
